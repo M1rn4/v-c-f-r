934da6189777f783dfb6e3e3654e2efe4729197a
Lurgia9072 <i202023015@cibertec.edu.pe>
2023-03-02 03:23:45 -0500

    connect API

--- components/verify.tsx
+++ components/verify.tsx
@@ -1,78 +1,83 @@
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  Button,
-  Textarea,
- 
-} from '@chakra-ui/react'
-
-import { Field, Formik, Form } from 'formik';
+import { useState } from 'react';
+import { Textarea, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure } from '@chakra-ui/react';
+import axios from 'axios';
 
 function VerifyText() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [text, setText] = useState('');
+  const [response, setResponse] = useState<any>();
 
-  function validateName(value: any) {
-    let error
-    if (!value) {
-      error = 'Is required'
-    } else if (value.toLowerCase() !== 'naruto') {
-      error = "verify!! text 😱"
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    try {
+      const responses = await axios.post('http://127.0.0.1:5000/predict', { data: text });
+      setResponse(responses.data);
+      onOpen();
+    } catch (error) {
+      console.error(error);
     }
-    return error
-  }
+  };
 
+  const handleTextareaChange = (event: any) => {
+    setText(event.target.value);
+  };
 
   return (
     <div>
-     <Formik
-      initialValues={{ name: 'Text' }}
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2))
-          actions.setSubmitting(false)
-        }, 1000)
-      }}
-    >
-      {(props) => (
-        <Form>
-          <Field name='name' validate={validateName}>
-            {({ field, form }: any) => (
-              <FormControl isInvalid={form.errors.name && form.touched.name}>
-                <FormLabel 
-                fontWeight={600}
-                fontSize={20}
-                >Send text</FormLabel>
-                <br />
-                <Textarea
-                 width="500px"
-                 borderRadius={10}
-                padding={10}
-                 color={'#464F41'}
-                 height="140px"
-                 fontSize={16}
-                 {...field} placeholder='Text' />
-                <FormErrorMessage color={'#D0C9C0'}>{form.errors.name}</FormErrorMessage>
-              </FormControl>
+      <form onSubmit={handleSubmit}>
+        <Textarea
+          width="500px"
+          borderRadius={10}
+          padding={10}
+          color={'#464F41'}
+          height="140px"
+          fontSize={16}
+          placeholder='Text'
+          value={text}
+          onChange={handleTextareaChange}
+        />
+        <Button
+          mt={8}
+          colorScheme='teal'
+          type='submit'
+          width="150px"
+          borderRadius={20}
+          border={'none'}
+          color={'#367E18'}
+          height="40px"
+          fontSize={16}
+          backgroundColor="#A8E890"
+        >
+          Submit
+        </Button>
+      </form>
+
+      <Modal
+        isCentered
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset='slideInBottom'
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {response && (
+              <div>
+                <p>adult: {response.adult}</p>
+                <p>no_adult: {response.no_adult}</p>
+              </div>
             )}
-          </Field>
-          <Button
-            mt={8}
-            colorScheme='teal'
-            isLoading={props.isSubmitting}
-            type='submit'
-            width="150px"
-            borderRadius={20}
-            border={'none'}
-            color={'#367E18'}
-            height="40px"
-            fontSize={16}
-            backgroundColor="#A8E890"
-          >
-            Submit
-          </Button>
-        </Form>
-      )}
-    </Formik>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
